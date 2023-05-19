ed220c5957e8ac4bfa0c4aa8ca0216c6eaf4e5ac
Jean-Luc Mounsamy <jl.mounsamy@gmail.com>
2023-05-19 16:12:04 -0400

    initial commit

---
+++ src/main/java/com/example/jpaawskmsencryptionpoc/JpaAwsKmsEncryptionPocApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaawskmsencryptionpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAwsKmsEncryptionPocApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(JpaAwsKmsEncryptionPocApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/jpaawskmsencryptionpoc/JpaAwsKmsEncryptionPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaawskmsencryptionpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaAwsKmsEncryptionPocApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
